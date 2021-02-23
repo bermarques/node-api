5078aa029bdbd448ebcdc2b20188d0e877ee4427
Bernardo Marques <bernardohmarques@hotmail.com>
2021-02-23 08:02:35 -0300

    chore: create GET and POST routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World - NLW04" });
+});
+
+app.post("/", (req, res) => {
+  return response.json({ message: "Os dados foram salvos com sucesso!" });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
